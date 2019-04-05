8ce001274592322381f1164c3b0198e213b1e190
DJMaster <developers@djmaster.com>
2019-04-05 08:42:17 +0200

    Updated to version 1.3.14

--- binding/enet.pas
+++ binding/enet.pas
@@ -24,7 +24,7 @@ const
 
   ENET_VERSION_MAJOR = 1;
   ENET_VERSION_MINOR = 3;
-  ENET_VERSION_PATCH = 13;
+  ENET_VERSION_PATCH = 14;
 
 
 {$ifdef MSWINDOWS}
@@ -524,6 +524,16 @@ function enet_socketset_select(socket: ENetSocket; readSet: PENetSocketSet; writ
 (** @defgroup Address ENet address functions
     @{
 *)
+(** Attempts to parse the printable form of the IP address in the parameter hostName
+    and sets the host field in the address parameter if successful.
+    @param address destination to store the parsed IP address
+    @param hostName IP address to parse
+    @retval 0 on success
+    @retval < 0 on failure
+    @returns the address of the given hostName in address on success
+*)
+function enet_address_set_host_ip(address: PENetAddress; const hostName: pchar): cint; cdecl; external LIB_ENET;
+
 (** Attempts to resolve the host named by the parameter hostName and sets
     the host field in the address parameter if successful.
     @param address destination to store resolved address
